c72b46f391203b6e25b52e0f1c70df83edfebb5f
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-06-24 14:55:26 -0430

    changes to the new example

--- examples/new/stuff.rb
+++ examples/new/stuff.rb
@@ -1,4 +1,5 @@
 a b c : Int
+a, b, c : Int
 
 read "give me a " a
 read "give me b " b
@@ -7,10 +8,21 @@ read "give me a ", a
 read "give me b ", b
 read "give me c ", c
 
-print "hola, a vale " a " y b + a vale " (b + a) " cuánto crees que vale b?"
-print "hola, a vale ", a " y b + a vale ", (b + a) " cuánto crees que vale b?"
+# to_String : _A a -> String
+# '_A' could be convention to "whatever type"
+# like 'a' and 'b' in map :: (a -> b) -> [a] -> [b]
 
-if a == 10 and b % 2 or c > 0
+# it can't be like Haskell because we name the variable next to the type
+# so it would be to_String : a a -> String
+# unless we make 'sugar' for when there's only a var name, it's a whatever type
+
+# print : [ _A ] -> IO ()
+print "hola, a vale " a " y b + a vale ", (b + a), " cuánto crees que vale b?"
+# print : [ String ] -> IO ()
+print "hola, a vale ", a " y b + a vale ", to_String(b + a), " cuánto crees que vale b?"
+
+if a == 10 and b % 2
+  or c > 0
     then
         print "sí"
     else
@@ -35,8 +47,8 @@ def g : Bool
   return true
 end
 
-def h : () -> Bool
-  return false
+def g_ : () -> Bool
+  return true
 end
 
 def i : Bool b -> ()
@@ -50,6 +62,9 @@ end
 def j : Bool b, Int i, Char c -> Char
   if b == (i % 2 == 0) then
     print c
+    # how do we write Char literals
+    # if we remove the conventional single quotes, and use a prefix "operator"
+    # we avoid users trying to write characters as strings, like 'abc'
     return 'a'
     return $a
     return &a
