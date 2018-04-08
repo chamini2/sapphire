e4b98e92ed9b3ae97cd3275ad7f320c302a4b214
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-04-08 01:50:53 -0400

    Add lexer identifiers specs

--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -10,6 +10,7 @@ import Lexer
 lexer = describe "lexer" $ do
     skippedChars
     comments
+    identifiers
 
 skippedChars = describe "skipped chars" $ do
     it "should skip spaces ' '" $ do
@@ -38,3 +39,36 @@ comments = describe "end-of-line comments" $ do
                 "stuff # comment\n" ++
                 "more"
         length res `shouldBe` 2 -- the 'stuff' and 'more' tokens
+
+identifiers = describe "identifiers" $ do
+    it "should consume an all-letters identifier" $ do
+        let res = scanTokens $
+                "iden"
+        res `shouldSatisfy` (\res -> case res of
+                [TkIdentifier val _] -> val == "iden"
+                _ -> False
+            )
+
+    it "should consume an underscore-starting identifier" $ do
+        let res = scanTokens $
+                "_iden"
+        res `shouldSatisfy` (\res -> case res of
+                [TkIdentifier val _] -> val == "_iden"
+                _ -> False
+            )
+
+    it "should consume an identifier with numbers" $ do
+        let res = scanTokens $
+                "iden2"
+        res `shouldSatisfy` (\res -> case res of
+                [TkIdentifier val _] -> val == "iden2"
+                _ -> False
+            )
+
+    it "should not consume a number-starting identifier" $ do
+        let res = scanTokens $
+                "1iden"
+        res `shouldSatisfy` (\res -> case res of
+                [_, TkIdentifier val _] -> val == "iden"
+                _ -> False
+            )
