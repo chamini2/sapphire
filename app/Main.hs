6d68da0f654511e78059dcd94944001ce455f48d
Matteo Ferrando <matteo.ferrando2@gmail.com>
2018-04-15 14:23:01 -0400

    better printing at the main program

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,19 @@
 module Main where
 
+import Control.Monad
+
 import Lib
 
 main :: IO ()
 main = do
     program <- getContents
     let tokens = scanTokens program
-    print $ tokens
+
+    putStrLn "TOKENS:"
+    forM_ tokens $ \tk -> do
+        putStr $ prettyShow (posn tk)
+        putStr ": "
+        putStrLn $ prettyShow tk
+
+    putStrLn "\nAST:"
     print $ parseProgram tokens
